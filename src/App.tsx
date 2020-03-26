a81dd67a181bc66d0617135c6e5a538ff5b2bf37
Simas <simas@simas-dei.com>
2020-03-26 19:43:33 +0200

    add navbar styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { CssBaseline } from '@material-ui/core';
 
 import Home from './components';
 
 export const App: React.FC = () => {
 	return (
 		<div className='App'>
+			<CssBaseline />
 			<Home />
 		</div>
 	);
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -4,6 +4,7 @@ import {
 	Toolbar,
 	List,
 	ListItem,
+	ListItemIcon,
 	ListItemText,
 	IconButton,
 	Avatar,
@@ -11,17 +12,90 @@ import {
 	Typography,
 	Box,
 } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import { ArrowBack, AssignmentInd, Home, Apps, ContactMail } from '@material-ui/icons';
 
+import avatar from '../../assets/avatar.png';
+
+const useStyles = makeStyles(theme => ({
+	navbar: {
+		background: '#222',
+	},
+	navbar__slider__container: {
+		width: 250,
+		background: '#511',
+		height: '30rem',
+	},
+	navbar__slider__item: {
+		color: 'tan',
+	},
+	navbar__slider__icon: {
+		color: 'tan',
+	},
+	navbar__avatar: {
+		display: 'block',
+		margin: '0.5rem auto',
+		width: theme.spacing(13),
+		height: theme.spacing(13),
+	},
+	navbar__title: {
+		color: 'tan',
+	},
+	navbar__icon: {
+		color: 'tomato',
+	},
+}));
+
+const menuItems = [
+	{
+		listIcon: <Home />,
+		listText: 'Home',
+	},
+	{
+		listIcon: <AssignmentInd />,
+		listText: 'Resume',
+	},
+	{
+		listIcon: <Apps />,
+		listText: 'Portfolio',
+	},
+	{
+		listIcon: <ContactMail />,
+		listText: 'Contacts',
+	},
+];
+
 export const Navbar: React.FC = () => {
+	const classes = useStyles();
 	return (
-		<Box component='nav'>
-			<AppBar>
-				<Toolbar>
-					<ArrowBack />
-				</Toolbar>
-			</AppBar>
-		</Box>
+		<>
+			<Box className={classes.navbar__slider__container} component='div'>
+				<Avatar className={classes.navbar__avatar} src={avatar} alt='Dude Bro' />
+				<Divider />
+				<List>
+					{menuItems.map(menuItem => (
+						<ListItem button key={menuItem.listText} className={classes.navbar__slider__item}>
+							<ListItemIcon className={classes.navbar__slider__icon}>
+								{menuItem.listIcon}
+							</ListItemIcon>
+							<ListItemText primary={menuItem.listText} />
+						</ListItem>
+					))}
+				</List>
+			</Box>
+			<Box component='nav'>
+				<AppBar position='static' className={classes.navbar}>
+					<Toolbar>
+						<IconButton>
+							<ArrowBack className={classes.navbar__icon} />
+						</IconButton>
+						<Typography variant='h5' className={classes.navbar__title}>
+							Tomato
+						</Typography>
+					</Toolbar>
+				</AppBar>
+			</Box>
+		</>
 	);
 };
 
