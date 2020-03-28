91df4431676a14139b410697433caba9bef04ce2
Simas <simas@simas-dei.com>
2020-03-28 19:11:35 +0200

    add particles js

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import { CssBaseline } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
 import coffeeApple from './assets/coffee-apple.jpg';
 
 import Home from './components';
 
-const style = {
-	display: 'flex',
-	'flex-direction': 'column',
-	height: '100vh',
-	width: '100wv',
-	background: `url(${coffeeApple}) no-repeat center center fixed`,
-};
+const useStyles = makeStyles({
+	app__container: {
+		display: 'flex',
+		'flex-direction': 'column',
+		height: '100vh',
+		width: '100wv',
+		background: `url(${coffeeApple}) no-repeat center center fixed`,
+	},
+});
 
 export const App: React.FC = () => {
+	const classes = useStyles();
 	return (
-		<div className='App' style={style}>
+		<div className={`${classes.app__container} App`}>
 			<CssBaseline />
 			<Home />
 		</div>
