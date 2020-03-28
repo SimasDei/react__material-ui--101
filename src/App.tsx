96c4fb99c562dae4ebfa5be8f302ab407854b978
Simas <simas@simas-dei.com>
2020-03-28 19:05:20 +0200

    add body styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import { CssBaseline } from '@material-ui/core';
 
+import coffeeApple from './assets/coffee-apple.jpg';
+
 import Home from './components';
+
+const style = {
+	display: 'flex',
+	'flex-direction': 'column',
+	height: '100vh',
+	width: '100wv',
+	background: `url(${coffeeApple}) no-repeat center center fixed`,
+};
 
 export const App: React.FC = () => {
 	return (
-		<div className='App'>
+		<div className='App' style={style}>
 			<CssBaseline />
 			<Home />
 		</div>
